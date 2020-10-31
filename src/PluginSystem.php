6f4934e5cf62f3698aadfa4c10c7281fea1b267f
isaeken <hello@isaeken.com.tr>
2020-10-31 20:15:08 +0300

    add comments

--- src/PluginSystem.php
+++ src/PluginSystem.php
@@ -1,15 +1,22 @@
 <?php
-
+/**
+ * Plugin System
+ * @version v1.0
+ * @author İsa Eken
+ * @license MIT
+ */
 
 namespace IsaEken\PluginSystem;
-
-
 
 use IsaEken\PluginSystem\Helpers\Str;
 use IsaEken\PluginSystem\Traits\PluginsExecuteTrait;
 use IsaEken\PluginSystem\Traits\PluginsManager;
 use IsaEken\PluginSystem\Traits\PluginsTrait;
 
+/**
+ * Class PluginSystem
+ * @package IsaEken\PluginSystem
+ */
 class PluginSystem
 {
     use PluginsTrait;
@@ -17,15 +24,24 @@ class PluginSystem
     use PluginsManager;
 
     /**
+     * Load a plugin file
+     *
      * @param string $pluginPath
      * @param mixed ...$attributes
      * @return Plugin
      */
     public function load(string $pluginPath, ...$attributes) : Plugin
     {
+        // include once plugin file with return
         $classname = include_once $pluginPath;
+
+        // if class name is not string then sets as filename
         if (!(is_string($classname) && strlen($classname) > 0)) $classname = pathinfo($pluginPath)['filename'];
+
+        // if extension is disabled extension then remove disabled parameter in filename
         if (Str::endsWith($pluginPath, '.disabled.php')) $classname = explode('.', $classname)[0];
+
+        // return plugin as class
         return new $classname($attributes);
     }
 }
